ccb1af4963d14e22343ca2a970d6d5b9ff3c6420
Julieth <juliethquinchia@gmail.com>
2020-02-27 22:03:24 -0500

    Validacion

--- validar.js
+++ validar.js
@@ -20,13 +20,13 @@ function validar() {
         alert("Hay campos vacios");
     }
     else if (Number(name_label)) {
-        alert("campo nombre incorrecto")
+        alert("campo nombre incorrecto");
     }
     else if (!expresion.test(email_label)) {
         alert("el email no es valido");
     }
     else if (isNaN(number_label)) {
-        alert("El campo number no es correcto")
+        alert("El campo number no es correcto");
     }
     else if (number_label.length < 6 || number_label.length > 20) {
         alert("numero invalido");
